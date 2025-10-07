03431950cc4955c5234da68a674c677f78ce251a
JulSannin <69036654+JulSannin@users.noreply.github>
2025-10-08 01:06:03 +0700

    refactor

--- script.js
+++ script.js
@@ -1,112 +1,85 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
-
-    let point = Math.floor(Math.random() * 3);
-
-    if (point === 0) {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
         return 'rock';
     }
-
-    else if (point === 1) {
-        return 'paper';
-    }
-
-    else {
+    else if (choice === 1) {
         return 'scissors';
     }
-
+    else {
+        return 'paper'
+    }
 }
 
 function getHumanChoice() {
-
-    let player_choice = window.prompt('Rock, paper or scissors');
-
-    if (player_choice === null) {
+    let choice = window.prompt('Rock, paper or scissors?');
+    if (choice === null) {
         return null;
     }
-
-    player_choice = player_choice.toLowerCase();
-
-    if (player_choice === 'rock' ||
-        player_choice === 'paper' ||
-        player_choice === 'scissors') {
-        return player_choice;
+    choice = choice.toLowerCase();
+    if (choice === 'rock'
+        || choice === 'scissors'
+        || choice === 'paper') {
+        return choice;
     }
-
     else {
         alert('Please enter only: rock, paper or scissors');
         return getHumanChoice();
     }
-
 }
 
-function playRound(humanChoice, computerChoice) {
+let computerScore = 0;
+let humanScore = 0;
 
+function playRound(humanChoice, computerChoice) {
     if (humanChoice === null) {
         alert('Game cancelled');
         return 'exit';
     }
-
-    let lowerHumanChoice = humanChoice.toLowerCase();
-    let lowerComputerChoice = computerChoice.toLowerCase();
-
-    if ((lowerHumanChoice === 'rock' && lowerComputerChoice === 'scissors') ||
-        (lowerHumanChoice === 'scissors' && lowerComputerChoice === 'paper') ||
-        (lowerHumanChoice === 'paper' && lowerComputerChoice === 'rock')) {
+    else if ((humanChoice === 'rock' && computerChoice == 'scissors') ||
+        (humanChoice === 'paper' && computerChoice == 'rock') ||
+        (humanChoice === 'scissors' && computerChoice == 'paper')) {
         humanScore += 1;
-        alert(`You win round! \n Human score: ${humanScore} \n Computer score: ${computerScore}`);
+        alert(`You win round, ${humanChoice} beat ${computerChoice} \n your score: ${humanScore} \n computer score: ${computerScore}`)
         return humanScore;
     }
-
-    else if ((lowerComputerChoice === 'rock' && lowerHumanChoice === 'scissors') ||
-        (lowerComputerChoice === 'scissors' && lowerHumanChoice === 'paper') ||
-        (lowerComputerChoice === 'paper' && lowerHumanChoice === 'rock')) {
+    else if ((computerChoice === 'rock' && humanChoice == 'scissors') ||
+        (computerChoice === 'paper' && humanChoice == 'rock') ||
+        (computerChoice === 'scissors' && humanChoice == 'paper')) {
         computerScore += 1;
-        alert(`You lose round! \n Human score: ${humanScore} \n Computer score: ${computerScore}`);
+        alert(`You lose round, ${computerChoice} beat ${humanChoice} \n your score: ${humanScore} \n computer score: ${computerScore}`)
         return computerScore;
     }
-
     else {
-        alert(`Tie! \n Human score: ${humanScore} \n Computer score: ${computerScore}`)
+        alert(`Tie \n your score: ${humanScore} \n computer score: ${computerScore}`)
         return null;
     }
-
 }
 
 function playGame() {
-
     if (humanScore === 5) {
         alert('You won game');
         return;
     }
-
     else if (humanScore < 5 && computerScore < 5) {
-
         let humanSelection = getHumanChoice();
         let computerSelection = getComputerChoice();
-
         let result = playRound(humanSelection, computerSelection);
-
         if (result === 'exit') {
             return;
         }
-
         console.log(humanScore, computerScore);
-
         playGame();
     }
-
     else {
-        alert('You lost game')
+        alert('You lost game');
+        return;
     }
 }
 
 function resetScores() {
     humanScore = 0;
     computerScore = 0;
-    return humanScore || computerScore;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
